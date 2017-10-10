3ef214d7f1174ea77c0e1ab15c752e06842d51a6
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2017-10-10 12:59:36 -0600

    Made code OCD friendly.

--- ButtonApp/ViewController.swift
+++ ButtonApp/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController
     private func createRandomColor() -> UIColor
     {
         let newColor :UIColor
-        let redValue :CGFloat = CGFloat(Double (arc4random_uniform(256)) / 255.0)
+        let redValue :CGFloat = CGFloat(Double (arc4random_uniform(256)) / 255.00)
         let greenValue :CGFloat = CGFloat(Double (arc4random_uniform(256)) / 255.00)
         let blueValue :CGFloat = CGFloat(Double (arc4random_uniform(256)) / 255.00)
         newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1.0))
