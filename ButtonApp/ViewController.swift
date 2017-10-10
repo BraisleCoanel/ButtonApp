22814cfd3448819b6766d09458f65e0f6816b871
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2017-10-10 12:17:17 -0600

    Created a button app that is the exact same as ThrowAwayApp.

---
+++ ButtonApp/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  ButtonApp
+//
+//  Created by Martinson, Branton on 10/10/17.
+//  Copyright © 2017 Martinson, Branton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+   
+    @IBOutlet weak var firstButton: UIButton!
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        if ( firstButton.backgroundColor == .purple)
+        {
+            firstButton.backgroundColor = .cyan
+        }
+        else
+        {
+            firstButton.backgroundColor = .purple
+        }
+        view.backgroundColor = createRandomColor()
+    }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redValue :CGFloat = CGFloat(Double (arc4random_uniform(256)) / 2.55)
+        let greenValue :CGFloat = CGFloat(Double (arc4random_uniform(256)) / 255.00)
+        let blueValue :CGFloat = CGFloat(Double (arc4random_uniform(256)) / 255.00)
+        newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1.0))
+        
+        
+        return newColor
+    }
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
